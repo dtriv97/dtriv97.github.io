a2065baa76b3f4001d32346b406920de1be2eca9
Dhairya Trivedi <Dhairya.Trivedi@fphcare.co.nz>
2023-03-22 12:45:47 +1300

    Add styling elements to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,28 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import React from 'react'
+import { GiHamburgerMenu as MenuIcon } from 'react-icons/gi'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-home-link">
-                <p>Home</p>
-            </Link>
-            <Link to="/#About" className="navbar-about-link">
-                <p>About</p>
-            </Link>
-            <Link to="/#Photography" className="navbar-photography-link">
-                <p>Photography</p>
-            </Link>
-            <Link to="/#Contact" className="navbar-contact-link">
-                <p>Contact</p>
-            </Link>
+            <MenuIcon className="navbar-menu-icon"/>
+            <ul>
+                <Link to="/" className="navbar-link">
+                    <li>Home</li>
+                </Link>
+                <Link to="/#About" className="navbar-link">
+                    <li>About</li>
+                </Link>
+                <Link to="/#Photography" className="navbar-link">
+                    <li>Photography</li>
+                </Link>
+                <Link to="/#Contact" className="navbar-link">
+                    <li>Contact</li>
+                </Link>
+            </ul>
         </div>
     </nav>
   )
