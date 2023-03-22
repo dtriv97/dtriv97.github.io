7045dadf41715443c617cd116bc5f184af2db82c
Dhairya Trivedi <dtriv97@gmail.com>
2023-03-23 00:38:06 +1300

    Create dynamic link generating

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,45 @@ import React from 'react'
 import { GiHamburgerMenu as MenuIcon } from 'react-icons/gi'
 import './Navbar.css'
 
+const linkList = [
+    {
+        name: 'Home',
+        link: "/"
+    },
+    {
+        name: "About",
+        link: "/#about"
+    },
+    {
+        name: "Photography",
+        link: "/#photography"
+    },
+    {
+        name: "Contact",
+        link: "/#contact"
+    }
+]
+
 function Navbar() {
-  return (
+
+    const [buttonToggle, buttonToggleClicked] = useState(false);
+
+    return (
     <nav className="navbar">
         <div className="navbar-container">
-            <MenuIcon className="navbar-menu-icon"/>
-            <ul>
-                <Link to="/" className="navbar-link">
-                    <li>Home</li>
-                </Link>
-                <Link to="/#About" className="navbar-link">
-                    <li>About</li>
-                </Link>
-                <Link to="/#Photography" className="navbar-link">
-                    <li>Photography</li>
-                </Link>
-                <Link to="/#Contact" className="navbar-link">
-                    <li>Contact</li>
-                </Link>
+            <MenuIcon className="navbar-menu-icon" viewBox="0 0 400 400"/>
+            <ul className="navbar-list">
+                {linkList.map((entry, idx) => {
+                    return (
+                        <Link to={entry.link} className = {
+                            idx !== (linkList.length - 1) ? 'navbar-link' : 'navbar-link-last'
+                        }>
+                            <li>
+                                {entry.name}
+                            </li>
+                        </Link>
+                    )
+                })}
             </ul>
         </div>
     </nav>
