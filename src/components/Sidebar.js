07d94d27471b0ba7e45f8a6aac9bbddd3dc7f62e
Dhairya Trivedi <dhairya.trivedi@fphcare.co.nz>
2023-03-29 12:51:23 +1300

    Sidebar links and styling changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import { React, useState } from 'react'
 import { MdMenu as MenuIcon, MdClose as MenuClose } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 import './Sidebar.css'
 
 function Sidebar({ links }) {
@@ -23,9 +24,9 @@ function Sidebar({ links }) {
 						links.map(item => {
 							return (
 								<li key={item.name}>
-									<a href={item.link}>
+									<Link to={item.link} onClick={menuClicked(!menuState)}>
 										{item.name}
-									</a>
+									</Link>
 								</li>
 							)
 						})
