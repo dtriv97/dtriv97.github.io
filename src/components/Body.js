9a110ed6ffd6fac1dd91216b49bc27c2b4b2b714
Dhairya Trivedi <dhairya.trivedi@fphcare.co.nz>
2023-04-02 15:52:55 +1200

    Added new body section

---
+++ src/components/Body.js
@@ -0,0 +1,17 @@
+import { React } from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='body-container'>
+            <div className='top-container'>
+
+            </div>
+            <div className="about-container">
+
+            </div>
+        </div>
+    )
+};
+
+export default Body;
