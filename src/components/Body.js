308f4898d72c7846bab5558c61252fe3b829e2d7
Dhairya Trivedi <dtriv97@gmail.com>
2023-04-02 23:10:43 +1200

    Changes to make body divs stack

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,11 +4,9 @@ import './Body.css'
 function Body() {
     return (
         <div className='body-container'>
-            <div className='top-container'>
-
+            <div className='home-container'>
             </div>
             <div className="about-container">
-
             </div>
         </div>
     )
