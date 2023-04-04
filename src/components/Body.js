0d40d0028bf54c637654b6379cc8719abf2d50fe
Dhairya Trivedi <dhairya.trivedi@fphcare.co.nz>
2023-04-04 12:53:15 +1200

    Update profile image

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import { React } from 'react'
 import './Body.css'
+import HomeImage from '../assets/home_photo.png'
 
 function Body() {
     return (
         <div className='body-container'>
             <div className='home-container'>
+                <img src={HomeImage} className="home-profile" alt="Home_Image"/>
             </div>
             <div className="about-container">
             </div>
