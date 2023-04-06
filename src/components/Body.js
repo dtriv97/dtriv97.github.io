7220a36734629b62dfe991b995ee15872108413b
Dhairya Trivedi <dtriv97@gmail.com>
2023-04-07 01:29:34 +1200

    Modify body styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,29 @@
 import { React } from 'react'
 import './Body.css'
-import HomeImage from '../assets/home_photo.png'
+import HomeImage from '../assets/home_photo.jpg'
 
 function Body() {
     return (
         <div className='body-container'>
-            <div className='home-container'>
-                <img src={HomeImage} className="home-profile" alt="Home_Image"/>
+            <div className='body-section home-container'>
+                <div className='column-container left'>
+                    <img src={HomeImage} className="home-profile" alt="Home_Image"/>
+                </div>
+                <div className="column-container right">
+                    <div className='blurb-container'>
+                        <p>
+                            Hi, my name is...
+                        </p>
+                    </div>
+                    <div className='name-container'>
+                        <p>
+                            DHAIRYA TRIVEDI
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div className="about-container">
+            <div className="body-section about-container">
+                <p>This is some content</p>
             </div>
         </div>
     )
