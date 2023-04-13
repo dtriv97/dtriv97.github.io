c40ea3e887fe772de4c6c8f0cbe8c4367ad6b4fc
Dhairya Trivedi <dtriv97@gmail.com>
2023-04-13 23:16:46 +1200

    Fix sidebar styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import Body from './components/Body';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+const linkList = [
+  {
+      name: 'Home',
+      link: "/"
+  },
+  {
+      name: "About",
+      link: "/#about"
+  },
+  {
+      name: "Photography",
+      link: "/#photography"
+  },
+  {
+      name: "Contact",
+      link: "/#contact"
+  }
+];
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Sidebar links={linkList} />
       <Body />
       <Routes>
         <Route path="/" exact/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,6 @@ import './Navbar.css'
 // import HeaderBar from "./HeaderBar"
 import Sidebar from "./Sidebar"
 
-const linkList = [
-    {
-        name: 'Home',
-        link: "/"
-    },
-    {
-        name: "About",
-        link: "/#about"
-    },
-    {
-        name: "Photography",
-        link: "/#photography"
-    },
-    {
-        name: "Contact",
-        link: "/#contact"
-    }
-]
 
 function Navbar() {
     // const windowDim = useWindowSize();
