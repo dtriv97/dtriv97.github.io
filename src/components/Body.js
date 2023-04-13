6a645c8a26e5887a0e654e75c45773698bfd9e15
Dhairya Trivedi <dtriv97@gmail.com>
2023-04-14 00:19:03 +1200

    Add topline on homepage

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,13 @@ function Body() {
     return (
         <div className='body-container'>
             <div className='body-section home-container'>
+                <div className='topline-container'>
+                    <p> Software Development </p>
+                    <p className='break'> | </p>
+                    <p> Data & Analytics </p>
+                    <p className='break'> | </p>
+                    <p> Landscape Photography </p>
+                </div>
                 <div className='img-container'>
                     <img src={HomeImage} className="home-profile" alt="Home_Image"/>
                 </div>
