a99c36d24c3a711593500be62f9776dcfdb207df
Dhairya Trivedi <dhairya.trivedi@fphcare.co.nz>
2023-04-21 16:00:07 +1200

    Updates

---
+++ precache-manifest.6b424bdcf1a520f8a14d6bcacebdb234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "233cd0acd475d8b033d7",
+    "url": "/static/css/main.6e37703b.chunk.css"
+  },
+  {
+    "revision": "233cd0acd475d8b033d7",
+    "url": "/static/js/main.233cd0ac.chunk.js"
+  },
+  {
+    "revision": "44a3a85295fe319758f4",
+    "url": "/static/js/1.44a3a852.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c63a86329c5f758463996a417831b614",
+    "url": "/static/media/profile-photo.c63a8632.png"
+  },
+  {
+    "revision": "ca105f24d86b809438a82567fe350159",
+    "url": "/static/media/background-b-w.ca105f24.jpg"
+  },
+  {
+    "revision": "0326ff9a7212e90824cbad2df4a8483f",
+    "url": "/index.html"
+  }
+];
