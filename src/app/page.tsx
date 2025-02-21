cba7cc6f531c44837225afc47c149851a6687ae7
Dhairya Trivedi <dhairya@grizzlysoftware.com>
2025-02-21 13:31:45 +1300

    Add below section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.mainContainer}>
+    <>
       <IntroCard />
-    </div>
+      <div className={styles.aboutContainer}>
+        <h2 className={styles.aboutTitle}>About Me</h2>
+        <p className={styles.aboutText}>
+          I am a software engineer, photographer and musician. I love to build
+          things, break things, solve things ... basically a curious mind. I am
+          passionate about music and photography and expressing myself through
+          them.
+        </p>
+      </div>
+    </>
   );
 }
