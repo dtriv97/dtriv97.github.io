91b9b3fa74059a365e2ddcbb1811e2c62f26a390
Dhairya Trivedi <dhairya@grizzlysoftware.com>
2025-02-21 13:32:02 +1300

    Navbar styling and fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
-import dtriv_logo from "../assets/dtriv_logo.png";
+import dtriv_logo from "../assets/DhairyaTrivediSignature.png";
+import githubLogoDark from "../assets/github_icon_black.svg";
+import githubLogo from "../assets/github_icon_white.svg";
+import instaLogoDark from "../assets/insta_icon_black.svg";
+import instaLogo from "../assets/insta_icon_white.svg";
+import linkedinLogoDark from "../assets/linkedin_icon_black.svg";
+import linkedinLogo from "../assets/linkedin_icon_white.svg";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
@@ -20,20 +27,45 @@ export default function Navbar() {
 
   return (
     <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ""}`}>
-      <div className={styles.logoContainer}>
-        <img src={dtriv_logo.src} className={styles.logo} />
+      <div className={styles.navbarWrapper}>
+        <div className={styles.logoContainer}>
+          {scrolled && <img src={dtriv_logo.src} className={styles.logo} />}
+        </div>
+        <ul className={styles.navLinks}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div className={styles.socialContainer}>
+          <Link
+            href="https://www.linkedin.com/in/dhairya-trivedi-44b356144/"
+            className={styles.socialLink}
+            target="_blank"
+          >
+            <img src={scrolled ? linkedinLogoDark.src : linkedinLogo.src} />
+          </Link>
+          <Link
+            href="https://github.com/dtriv97"
+            className={styles.socialLink}
+            target="_blank"
+          >
+            <img src={scrolled ? githubLogoDark.src : githubLogo.src} />
+          </Link>
+          <Link
+            href="https://www.instagram.com/dtriv97/"
+            className={styles.socialLink}
+            target="_blank"
+          >
+            <img src={scrolled ? instaLogoDark.src : instaLogo.src} />
+          </Link>
+        </div>
       </div>
-      <ul className={styles.navLinks}>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
     </nav>
   );
 }
